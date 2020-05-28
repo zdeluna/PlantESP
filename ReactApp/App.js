e311a33ccea19d14b09aa94ff7e2998b2cc368ea
Zachary DeLuna <zach.deluna@gmail.com>
2020-05-28 16:40:36 -0500

    Add apollo client configuration to entry point

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -8,107 +8,82 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {createHttpLink} from 'apollo-link-http';
+import {setContext} from 'apollo-link-context';
+import {onError} from 'apollo-link-error';
+import {ApolloProvider} from '@apollo/react-hooks';
+import {from} from 'apollo-boost';
+import {Home} from './Home';
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+let GRAPHQL_URI =
+    'https://bj6gqabbda.execute-api.us-east-2.amazonaws.com/dev/graphql';
+
+const httpLink = createHttpLink({
+    uri: GRAPHQL_URI,
+    headers: {
+        'client-name': 'Plant ESP [React App]',
+        'client-version': '1.0.0',
+    },
 });
+
+const errorLink = onError(({graphQLErrors, networkError}) => {
+    if (graphQLErrors)
+        graphQLErrors.map(({message, locations, path}) => {
+            // If the user is not authenticated, remove the token in local storage which will proceed the user to the login screen
+            if (message === '401: Unauthorized') {
+                localStorage.setItem('token', null);
+                client.cache.reset();
+                app.auth().signOut();
+            }
+            console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            );
+        });
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const authLink = setContext((_, {headers}) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const cache = new InMemoryCache({dataIdFromObject: object => object.id});
+const client = new ApolloClient({
+    cache,
+    link: from([authLink, errorLink, httpLink]),
+});
+
+const App: () => React$Node = () => {
+    return (
+        <ApolloProvider client={client}>
+            <Home />
+        </ApolloProvider>
+    );
+};
 
 export default App;
